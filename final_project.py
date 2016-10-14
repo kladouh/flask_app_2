6c7362c0416d2851c2adff5092600e2c40c1b3f4
KL <karimlahoud@Karims-MacBook-Pro.local>
2016-10-14 00:20:45 -0400

    final touches

--- final_project.py
+++ final_project.py
@@ -2,34 +2,40 @@ from flask import Flask, render_template, request
 import giphypop
 import os
 
-app = Flask(__name__)
+#Sets up the flask module
+app = Flask(__name__) 
 
 
+#Registers url of main page
 @app.route('/')
 def index():
 	return render_template('index.html')  
 
 
-
+#Registers url of about page
 @app.route("/about")
 def about():
 	return render_template('about.html')
 
 
+#Registers url of results page
 @app.route('/results')
 def results():
+
+	#Gets the variable 'gif_name' (an input variable on the main page)
 	gif_name = request.values.get('gif_name')
+	
+	#Runs a giphy search for 'gif_name'
 	g = giphypop.Giphy()
 	results = g.search_list(gif_name)
 
+	#Passes the variables 'results' and 'gif_name' to the html code
 	return render_template('results.html', gif_name=gif_name, results=results)
 
 
 
-
-
-
 if __name__ == "__main__": 
+	#Necessary to deploy to Heroku
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port)
 
@@ -39,4 +45,4 @@ if __name__ == "__main__":
 
 # To do list
 #1- Edit your README.md file so that it links to your newly created Heroku app page and push that to Github.
-#2- Add comments
+#3- rename heroku address
